177e8ce3980d116b5d5a7b20800a852c4ab00134
giri1987 <p.girish8@gmail.com>
2017-11-16 09:49:01 +0530

    3-way sort
    removed print statemnts..

--- 3-way-Qsort.py
+++ 3-way-Qsort.py
@@ -1,16 +1,10 @@
-
-
-
-
-
-
 # -*- coding: utf-8 -*-
 """
 Created on Tue Nov 14 12:57:25 2017
 
 @author: gpillai082916
 """
-
+n=int(raw_input())
 vals=map(int,raw_input().split())
 def swap(l,r):
  global vals
@@ -23,14 +17,14 @@ def swap(l,r):
 def partition3(l,r):
  global vals
  dup=vals[l:r]
- print(dup)
+ #print(dup)
  
- print("l,r :",(l,r))
+ #print("l,r :",(l,r))
  mid=len(dup)/2
  
- print("mid_pos: ", mid)
+ #print("mid_pos: ", mid)
  m_val=dup[mid]
- print("mid_value:",m_val)
+ #print("mid_value:",m_val)
  m_arr=[]
  l_arr=[]
  r_arr=[]
@@ -44,7 +38,7 @@ def partition3(l,r):
    r_arr.append(i)
  
  vals[l:r]=l_arr+m_arr+r_arr
- print("vals :",vals)
+ #print("vals :",vals)
  m1=l+len(l_arr)
  if m1 < 0 :
      m1=0
@@ -57,7 +51,7 @@ def partition3(l,r):
 #Regular Partitioning
 def partition(l,r):
  global vals
- print(vals)   
+ #print(vals)   
  pivot=vals[l]
  j=l
  for i in range((l+1),r+1):
@@ -74,7 +68,7 @@ def quickSort(l,r):
  if l>=r:
   return
  m=partition(l,r)
- print("m:",m)
+ #print("m:",m)
  quickSort(l,m-1)
  quickSort((m+1),r)
 
@@ -84,14 +78,17 @@ def quickSort3(l,r):
  if l>=r:
      return
  (m1,m2)=partition3(l,r)
- print("m's :",(m1,m2))
+ #print("m's :",(m1,m2))
  quickSort3(l,m1)
  quickSort3(m2,r) 
  
 def main():
  global vals   
  quickSort3(0,len(vals))
- print(vals)
+ #print(vals)
 
 main()   
     
+
+
+
